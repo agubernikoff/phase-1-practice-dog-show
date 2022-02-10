7cc2ca7c142f65981a5a19cc73d61dec969047cb
alexander gubernikoff <agubernikoff@gmail.com>
2022-02-10 02:59:18 -0500

    not much progress maybe went backwards

--- src/index.js
+++ src/index.js
@@ -16,8 +16,11 @@ document.addEventListener("DOMContentLoaded", () => {
     tableBody.append(newRow);
     newRow.append(newNameData, newBreedData, newSexData, newEditDogData);
     newNameData.innerText = dog.name;
+    newNameData.id = dog.name;
     newBreedData.innerText = dog.breed;
+    newBreedData.id = dog.name + dog.id;
     newSexData.innerText = dog.sex;
+    newSexData.id = dog.sex + dog.id;
     newEditDogData.append(editDogBttn);
     editDogBttn.addEventListener("click", function () {
       editDoge(dog);
@@ -42,16 +45,39 @@ document.addEventListener("DOMContentLoaded", () => {
       newDogObj.name = nameInput.value;
       newDogObj.breed = breedInput.value;
       newDogObj.sex = sexInput.value;
+      //const nameCell = document.querySelector(`#${dog.name}`);
+      //const breedCell = document.querySelector(`#${dog.name + dog.id}`);
+      //const sexCell = document.querySelector(`#${dog.sex + dog.id}`);
+      //nameCell.innerText = newDogObj.name;
+      //breedCell.innerText = newDogObj.breed;
+      //sexCell.innerText = newDogObj.sex;
+
       fetch(`http://localhost:3000/dogs/${dog.id}`, {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(newDogObj),
       })
         .then((resp) => resp.json())
-        .then((data) => data);
-      fetch(`http://localhost:3000/dogs`)
-        .then((resp) => resp.json())
-        .then((data) => data.forEach((dog) => renderDog(dog)));
+        .then((data) => reFetch(data));
+      function reFetch(data) {
+        data.addEventListener("ContentLoaded", () => {
+          console.log("worked");
+          /*fetch("http://localhost:3000/dogs")
+            .then((resp) => resp.json())
+            .then((data) => data.forEach((dog) => reRenderDog(dog)));*/
+        });
+      }
+      function reRenderDog(dog) {
+        const nameCell = document.querySelector(`#${dog.name}`);
+        const breedCell = document.querySelector(`#${dog.name + dog.id}`);
+        const sexCell = document.querySelector(`#${dog.sex + dog.id}`);
+        console.log(nameCell, breedCell, sexCell);
+        console.log(dog.name, dog.breed, dog.sex);
+        nameCell.innerText = dog.name;
+        breedCell.innerText = dog.breed;
+        sexCell.innerText = dog.sex;
+      }
+      ///
 
       form.reset();
     });
